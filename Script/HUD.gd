92b0874045edaddada6ec1ac1c2a479b4acd02c0
Aerann <neonath78@gmail.com>
2024-09-25 18:54:17 +0200

    ajout du hud stylisé

--- Script/HUD.gd
+++ Script/HUD.gd
@@ -8,7 +8,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	$"cendre_gelée".text = str($"../Player".cendre_gelee)
+	$health_bar.set_value_no_signal($"../Player".health)
 
 
 func _on_player_hit():
-	$health_bar.set_value_no_signal($"../Player".health)
+	pass
