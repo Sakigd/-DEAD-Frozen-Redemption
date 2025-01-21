bcee0f0891a5b6f0ddf8e94c7350c4cf8499edef
Kiapigeon <vinhfabriano@gmail.com>
2025-01-21 22:33:28 +0100

    scene setup

---
+++ Igris/igris_boss_behavior.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+var health : float = 100
+var hit_tween : Tween = null
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	pass
+
+
+func _hit(value:float=10) :
+	health -= value
+	$Sprite.self_modulate.g = 0.6
+	$Sprite.self_modulate.b = 0.7
+	hit_tween = create_tween()
+	hit_tween.parallel().tween_property($Sprite,"self_modulate:g",1.0,0.3)
+	hit_tween.parallel().tween_property($Sprite,"self_modulate:b",1.0,0.3)
+
+
+func _on_hitbox_area_entered(area):
+	if area.is_in_group("hitbox_ruban"):
+		_hit()
