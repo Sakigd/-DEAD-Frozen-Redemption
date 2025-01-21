a9e6e93f9241e18ccd1bcbf286556411f44864d4
Kiapigeon <vinhfabriano@gmail.com>
2025-01-22 00:24:27 +0100

    brutal front and back

--- Igris/igris_boss_behavior.gd
+++ Igris/igris_boss_behavior.gd
@@ -9,11 +9,16 @@ extends CharacterBody2D
 var health : float = 100
 var hit_tween : Tween = null
 var direction : int = -1
-enum s {IDLE,FLIP}
+enum s {IDLE,FLIP,BRUTAL,BRUTAL_BACK,DASH}
 var state : int = s.IDLE
 var state_time = 0
 var player_in_small_range : bool
 var player_in_big_range : bool
+const flip_length : float = 0.4
+const brutal_length : float = 1.0
+const brutal_hit_start : float = 0.75
+const brutal_hit_duration : float = 0.15
+const dash_length : float = 1.0
 
 
 func _ready():
@@ -35,14 +40,39 @@ func _physics_process(delta):
 	if old_state != state :
 		state_time = 0
 	print("s=",state,"   t=",snapped(state_time,0.001))
+	
+	$Attacks/Brutal.disabled = not (state == s.BRUTAL and state_time >= brutal_hit_start \
+		and state_time <= brutal_hit_start+brutal_hit_duration)
+	$Attacks/BrutalBack.disabled = not (state == s.BRUTAL_BACK and state_time >= brutal_hit_start \
+		and state_time <= brutal_hit_start+brutal_hit_duration)
+	
+	for attack_box in $Attacks.get_children() :
+		attack_box.position.x = abs(attack_box.position.x) * direction
+		if attack_box.name.contains("Back") :
+			attack_box.position.x *= -1
 
 
 func _transitions() -> s :
-	if not player_in_small_range and not _facing_player() :
+	if not player_in_small_range and not _facing_player() and state == s.IDLE :
 		direction = -1 * direction
 		return s.FLIP
-	if s.FLIP and state_time >= 0.4 :
+	
+	if state == s.FLIP and state_time >= flip_length :
 		return s.IDLE
+	
+	if state == s.IDLE and player_in_small_range :
+		if _facing_player() :
+			return s.BRUTAL
+		else :
+			return s.BRUTAL_BACK
+	
+	if state == s.BRUTAL or state == s.BRUTAL_BACK :
+		if state_time >= brutal_length :
+			return s.IDLE
+	
+	if player_in_big_range :
+		return s.DASH
+	
 	return state
 
 
