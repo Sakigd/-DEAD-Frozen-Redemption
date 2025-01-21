6a49853cfedbedf62f88738908913ae9de288bdc
Kiapigeon <vinhfabriano@gmail.com>
2025-01-21 23:22:06 +0100

    player in front check

--- Igris/igris_boss_behavior.gd
+++ Igris/igris_boss_behavior.gd
@@ -5,15 +5,18 @@ extends CharacterBody2D
 	#Animations
 	#Discovery cutscene*
 
+@export var player : Node2D = null
 var health : float = 100
 var hit_tween : Tween = null
+var direction : int = -1
 
 func _ready():
 	pass
 
 
-func _process(delta):
-	pass
+func _physics_process(delta):
+	$Sprite.flip_h = (direction == 1)
+	print(_player_in_front())
 
 
 func _hit(value:float=10) :
@@ -30,3 +33,13 @@ func _hit(value:float=10) :
 func _on_hitbox_area_entered(area):
 	if area.is_in_group("hitbox_ruban"):
 		_hit()
+
+
+func _player_in_front() :
+	var to_player : float = player.global_position.x - global_position.x
+	if to_player == 0 :
+		return true
+	if sign(to_player) == direction :
+		return true
+	else :
+		return false
