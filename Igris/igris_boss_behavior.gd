23d1448d42c32b7024c05180d1c6be091b8e3a44
Kiapigeon <vinhfabriano@gmail.com>
2025-01-21 23:51:41 +0100

    flip state

--- Igris/igris_boss_behavior.gd
+++ Igris/igris_boss_behavior.gd
@@ -5,18 +5,45 @@ extends CharacterBody2D
 	#Animations
 	#Discovery cutscene*
 
-@export var player : Node2D = null
+@export var player : CharacterBody2D = null
 var health : float = 100
 var hit_tween : Tween = null
 var direction : int = -1
+enum s {IDLE,FLIP}
+var state : int = s.IDLE
+var state_time = 0
+var player_in_small_range : bool
+var player_in_big_range : bool
+
 
 func _ready():
-	pass
+	if not player :
+		queue_free()
 
 
 func _physics_process(delta):
 	$Sprite.flip_h = (direction == 1)
-	print(_player_in_front())
+	
+	player_in_small_range = $SmallRange.get_overlapping_bodies().has(player)
+	player_in_big_range = $BigRange.get_overlapping_bodies().has(player)
+	if player_in_big_range and player_in_small_range :
+		player_in_big_range = false
+	
+	state_time += delta
+	var old_state = state
+	state = _transitions()
+	if old_state != state :
+		state_time = 0
+	print("s=",state,"   t=",snapped(state_time,0.001))
+
+
+func _transitions() -> s :
+	if not player_in_small_range and not _facing_player() :
+		direction = -1 * direction
+		return s.FLIP
+	if s.FLIP and state_time >= 0.4 :
+		return s.IDLE
+	return state
 
 
 func _hit(value:float=10) :
@@ -35,7 +62,7 @@ func _on_hitbox_area_entered(area):
 		_hit()
 
 
-func _player_in_front() :
+func _facing_player() :
 	var to_player : float = player.global_position.x - global_position.x
 	if to_player == 0 :
 		return true
